d5fe0afd40cacd15b953962bbcd8d3ebe370f11b
Marcel Koch <marcel.koch@web.de>
2015-04-12 12:11:45 +0200

    Adds task 4 from sheet 1. NO INPUT/OUTPUT

--- blatt1.cpp
+++ blatt1.cpp
@@ -1,10 +1,42 @@
+#include <iostream>
+#include <cmath>
+
 #include "matrix.h"
 #include "vector.h"
 #include "real.h"
 
-#include <iostream>
 
 int main(int argc, char **argv) {
+  Vector x{10}, y{10};
+  Matrix A{10,10}, gradientMatrix{60,40}, sinusMatrix{60,40};
+  Matrix U, V;
 
-	return 0;
+  // Initialize A with A_ij = (i+1)*(j+1)
+  for(unsigned i=0; i<10; ++i) {
+    for(unsigned j=0; j<10; ++j) {
+      A.set(i, j, (i+1)*(j+1));
+    }
+  }
+
+  // Initialize x and y with x=1 and y=0
+  for(unsigned i=0; i<10; ++i) {
+    x.set(i, 1.);
+    y.set(i, 0.);
+  }
+
+  // Display A 
+  
+  // Initialize gradient matrix and sinus matrix
+  for(unsigned i=0; i<60; ++i) {
+    for(unsigned j=0; j<40; ++j) {
+      gradientMatrix.set(i, j, M_PI * i / 60.);
+      sinusMatrix.set(i, j, sin(i));
+    }
+  }
+
+  // Read matrices U and V
+
+  // Print U and V to .vtk
+
+  return 0;
 }
