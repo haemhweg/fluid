c2708d2b9981501df6b4b78eef1b21b5989f712f
Marcel <marcel.koch@web.de>
2015-04-23 23:28:44 +0200

    Parametereingabe hinzugefügt, KEINE AUSREICHENDE FEHLERBEHANDLUNG

--- include/config.h
+++ include/config.h
@@ -1,53 +1,48 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
+#include <string>
+
 #include "real.h"
 
-class Config
+struct Config
 {
-	/**
-	 * Nur ein Vorschlag. Eigentlich muss man hier den Config irgendwie enkapsulieren.
-	 */
-public:
-	struct geo
-	{
-		REAL xlength;
-		REAL ylength;
-		int imax;
-		int jmax;
-		REAL delx;
-		REAL dely;
-	};
-	struct time
-	{
-		REAL t;
-		REAL t_end;
-		REAL delt;
-		REAL tau;
-		REAL del_vec;
-	};
-	struct pressure
-	{
-		int itermax;
-		int it;
-		REAL res;
-		REAL eps;
-		REAL omg;
-		REAL alpha;
-	};
-	struct constants
-	{
-		REAL Re;
-		REAL GX, GY;
-		REAL UI, VI, PI;
-	};
+  struct geo
+  {
+    REAL xlength;
+    REAL ylength;
+    unsigned imax;
+    unsigned jmax;
+    REAL delx;
+    REAL dely;
+  };
+  struct time
+  {
+    REAL t_end;
+    REAL delt;
+    REAL tau;
+    REAL del_vec;
+  };
+  struct solver
+  {
+    int itmax;
+    REAL eps;
+    REAL omega;
+    REAL alpha;
+  };
+  struct constants
+  {
+    REAL Re;
+    REAL GX, GY;
+    REAL UI, VI, PI;
+  };
 
-	geo _geo;
-	time _time;
-	pressure _pressure;
-	constants _constants;
+  geo _geo=geo();
+  time _time=time();
+  solver _solver=solver();
+  constants _constants=constants();
 
-	Config(char * filename);
+  Config(const std::string& filename);
 };
 
 #endif
